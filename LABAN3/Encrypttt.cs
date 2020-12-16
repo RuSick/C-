5ea677cbadee1ab230e175cd2eda7c0a9f3a9944
RuSick <66710649+RuSick@users.noreply.github.com>
2020-12-16 08:54:54 +0300

    Add files via upload

---
+++ LABAN3/Encrypttt.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABA3
+{
+    class Encrypttt
+    {
+        static public string EncodeTo64(string path, string targetDir)
+        {
+            string txt;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                txt = sr.ReadToEnd();
+            }
+            byte[] toEncodeAsBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(txt);
+            string returnValue = System.Convert.ToBase64String(toEncodeAsBytes);
+            string newpath = targetDir + @"\temp\encoded.txt";
+            using (FileStream fstream = new FileStream(newpath, FileMode.Create))
+            {
+                byte[] array = System.Text.Encoding.Default.GetBytes(returnValue);
+                fstream.Write(array, 0, array.Length);
+            }
+            return newpath;
+
+        }
+        //DECODER
+        static public void DecodeFrom64(string path)
+        {
+            string txt;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                txt = sr.ReadToEnd();
+
+            }
+            byte[] encodedDataAsBytes = System.Convert.FromBase64String(txt);
+            string returnValue = System.Text.ASCIIEncoding.ASCII.GetString(encodedDataAsBytes);
+            string newpath = path + "_UNencoded.txt";
+            using (FileStream fstream = new FileStream(newpath, FileMode.OpenOrCreate))
+            {
+                byte[] array = System.Text.Encoding.Default.GetBytes(returnValue);
+                fstream.Write(array, 0, array.Length);
+            }
+
+        }
+    }
+}
---
+++ LABAN3/Pressets.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+
+namespace LABA3
+{
+    class Pressets
+    {
+        [JsonPropertyName("Source_Directory")]
+        internal string sourceDir { get; set; } = @"C:\Users\User\source\repos\C#\2 kurs\2\SourceDir";
+        [JsonPropertyName("Target_Directory")]
+        internal string targetDir { get; set; } = @"C:\Users\User\source\repos\C#\2 kurs\2\TargetDir";
+        [JsonPropertyName("Dearchive_Directory")]
+        internal string DearchiveDir { get; set; } = @"C:\Users\User\source\repos\C#\2 kurs\2\TargetDir\Dearchive";
+        public Pressets() { }
+    }
+    class SettingsParser
+    {
+        public Pressets SettingsXML()
+        {
+            Pressets parsed = new Pressets();
+            if (File.Exists("conf.xml"))
+            {
+                XmlDocument xml = new XmlDocument();
+                xml.Load("conf.xml");
+                foreach (XmlNode n in xml.SelectNodes("Settings"))
+                {
+                    string Sdir = n.SelectSingleNode("sourceDir").InnerText;
+                    string Tdir = n.SelectSingleNode("targetDir").InnerText;
+                    string Ddir = n.SelectSingleNode("DearchivetDir").InnerText;
+                    parsed.sourceDir = Sdir;
+                    parsed.targetDir = Tdir;
+                    parsed.DearchiveDir = Ddir;
+                }
+            }
+            return parsed;
+
+        }
+        public Pressets SettingsJson()
+        {
+            Pressets parsed = new Pressets();
+            if (File.Exists("Setting.json"))
+            {
+                parsed = JsonConvert.DeserializeObject<Pressets>(File.ReadAllText("pressets.json"));    
+            }
+            return parsed;
+        }
+    }
+}
